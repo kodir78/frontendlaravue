27c82b3ded9477e7d9eb6105b773e1cbdc483b3e
kodir78 <kodir.petani@gmail.com>
2020-05-10 14:40:35 +0800

    first commit Laravue Frontend

---
+++ src/components/PatnerShayna.vue
@@ -0,0 +1,47 @@
+<template>
+<!-- Women Banner Section Begin -->
+    <section class="partner-logo">
+        <div class="container">
+                    <carousel class="logo-carousel" :items="5" :dots="false" :nav="false" :autoplay="true" :loop="true">
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img src="img/logo-carousel/logo-1.png" alt="" />
+                            </div>
+                        </div>
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img src="img/logo-carousel/logo-2.png" alt="" />
+                            </div>
+                        </div>
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img src="img/logo-carousel/logo-3.png" alt="" />
+                            </div>
+                        </div>
+                        <div class="logo-item">
+                            <div class="tablecell-inner">
+                                <img src="img/logo-carousel/logo-4.png" alt="" />
+                            </div>
+                        </div>
+                    </carousel>
+        </div>
+    </section>
+    <!-- Women Banner Section End -->
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+
+export default {
+  name: "PatnerShayna",
+  components: {
+      carousel
+  }
+}
+</script>
+
+<style scoped>
+.product-item{
+    margin-right: 25px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HeaderShayna />
+    <HeroShayna />
+    <SliderShayna />
+    <InstaShayna />
+    <PatnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue"
+import HeroShayna from "@/components/HeroShayna.vue"
+import SliderShayna from "@/components/SliderShayna.vue"
+import InstaShayna from "@/components/InstaShayna.vue"
+import PatnerShayna from "@/components/PatnerShayna.vue"
+import FooterShayna from "@/components/FooterShayna.vue"
+
+export default {
+  name: "home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    SliderShayna,
+    InstaShayna,
+    PatnerShayna,
+    FooterShayna
+  }
+}
+</script>
